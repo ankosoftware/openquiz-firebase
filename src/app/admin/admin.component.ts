c3a7c636242c048e353f9f4b158a7bc51ae406e9
Eugene Kandlen <ekandlen@kibibox.com>
2017-04-22 22:33:45 +0700

    fix user

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {AuthService} from "../common/firebase/services/auth.service";
 
 @Component({
   templateUrl: './admin.component.html'
 })
 export class AdminComponent {
-  user: any;
+  @Input() user;
   constructor(private authService: AuthService) {
-    this.authService.getUser().then((user)=> {
-      this.user = user;
-    });
+    console.log('Trace Admin component', this.user);
   }
 }
