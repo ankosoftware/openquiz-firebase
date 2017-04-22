14640be60b1bac4ccbdff1b491cba1db14b9d992
Vladislav Zubairov <vzubairov@ankocorp.com>
2017-04-23 00:43:14 +0700

    WIP

--- src/app/admin/questions/question.component.ts
+++ src/app/admin/questions/question.component.ts
@@ -1,15 +1,63 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { QuestionService } from '../../common/firebase/services/question.service';
+import { Question, QuestionType, QuestionAnswer } from '../../common/model/question.model';
 
 @Component({
   templateUrl: './question.component.html'
 })
 export class QuestionComponent {
-  question = {
-    answerType: 'multiple-choice'
+  question: Question | any = {
+    type: QuestionType.single
   };
+
   answers = [];
 
+  constructor(
+    protected questionService: QuestionService
+  ) {}
+
+  onControlsChanged(index: number) {
+    if (this.question.type === QuestionType.single) {
+      this.answers.forEach((answer, indexInThePool: number) => {
+        if (indexInThePool !== index) {
+          answer.isCorrect = false;
+        }
+      });
+    }
+  }
+
   addAnswer() {
-    this.answers.push({});
+    this.answers.push({
+      _timestamp: new Date().getTime()
+    });
+  }
+
+  removeAnswer(index) {
+    this.answers.splice(index, 1);
+  }
+
+  onSubmit(form: NgForm, event) {
+    event.preventDefault();
+    if (!form.valid) {
+      return;
+    }
+
+    const { question } = this;
+    const payloadData = new Question({
+      name: question.name,
+      type: question.type,
+      description: question.description,
+      answers: this.answers.map((answer) => {
+        return new QuestionAnswer({
+          answer: answer.answer,
+          isCorrect: answer.isCorrect
+        });
+      })
+    });
+
+    this.questionService.create(payloadData).then((response) => {
+      console.log(response);
+    });
   }
 }
--- src/app/common/model/question.model.ts
+++ src/app/common/model/question.model.ts
@@ -1,16 +1,26 @@
 import {Base} from "./base.model";
 
-export class Question extends Base {
-
-  name: string;
-  // TODO: enum
-  type: string;
-  description: string;
-  answers: string[];
-  correctAnswer: string;
+export class QuestionAnswer extends Base {
+  answer: string;
+  isCorrect: boolean;
 
   constructor(json?: any) {
     super(json);
   }
+}
 
+export enum QuestionType {
+  single = 1,
+  multipleChoice,
+}
+
+export class Question extends Base {
+  name: string;
+  type: QuestionType;
+  description: string;
+  answers: QuestionAnswer[];
+
+  constructor(json?: any) {
+    super(json);
+  }
 }
