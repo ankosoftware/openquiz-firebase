2420fa03b8a22b24484dd4510e061475532da29d
Vladislav Zubairov <vzubairov@ankocorp.com>
2017-04-22 18:17:34 +0700

    Quizzes

---
+++ src/app/admin/quizzes/new-quiz.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { DialogComponent } from 'ng2-bootstrap-modal';
+
+@Component({
+  templateUrl: './new-quiz.component.html'
+})
+export class NewQuizComponent extends DialogComponent<any, any> {
+
+}
--- src/app/admin/quizzes/quizzes.component.ts
+++ src/app/admin/quizzes/quizzes.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { DialogService } from 'ng2-bootstrap-modal';
+import { NewQuizComponent } from './new-quiz.component';
 
 @Component({
   templateUrl: './quizzes.component.html'
 })
 export class QuizzesComponent {
+  constructor(
+    protected dialogService: DialogService
+  ) {}
 
+  newQuiz() {
+    this.dialogService.addDialog(NewQuizComponent, {});
+  }
 }
