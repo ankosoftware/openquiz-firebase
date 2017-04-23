09006c976059b84077d7ede725275aa0c73e7dce
Vladislav Zubairov <vzubairov@ankocorp.com>
2017-04-23 14:44:45 +0700

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {AuthService} from "./common/firebase/services/auth.service";
-
-declare const $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  ngAfterViewInit() {
-    setTimeout(() => {
-      $.material.init();
-    });
-  }
-}
+export class AppComponent {}
