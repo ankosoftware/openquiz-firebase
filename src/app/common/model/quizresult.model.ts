a01e7137f89064b0df7fdd6ddf1617f220aef8c2
Eugene Kandlen <ekandlen@kibibox.com>
2017-04-24 00:10:48 +0700

    create quiz result on start

--- src/app/common/model/quizresult.model.ts
+++ src/app/common/model/quizresult.model.ts
@@ -1,18 +1,29 @@
+import uuid from "uuid";
 import {Base} from "./base.model";
-import {Quiz} from "./quiz.model";
 
+export class ResultAnswer extends Base {
+  question: string; // id
+  entered: string[]; // entered answers
+
+  constructor(json?: any) {
+    super(json);
+    this.id = this.id || uuid.v4();
+  }
+
+}
 export class QuizResult extends Base {
 
-  quiz: Quiz;
-  user: string; // id or email
+  quiz: string; // id
+  user: string; // email, owner email
   start: Date;
   end: Date;
-  answers: string[];
+  answers: ResultAnswer[];
   score: number;
   maxScore: number;
 
   constructor(json?: any) {
     super(json);
+    this.answers = this.answers || [];
   }
 
 }
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,13 +1,15 @@
-import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
-import { UIRouter } from "ui-router-ng2";
-import { User } from "../common/model/user.model";
-import { MaterialComponent } from '../common/components/material/material.component';
+import {ChangeDetectorRef, Component, Input, OnInit} from "@angular/core";
+import {UIRouter} from "ui-router-ng2";
+import {User} from "../common/model/user.model";
+import {MaterialComponent} from "../common/components/material/material.component";
 import {Quiz} from "../common/model/quiz.model";
 import {Topic} from "../common/model/topic.model";
 import {TopicService} from "../common/firebase/services/topic.service";
 import {Question} from "../common/model/question.model";
 import {QuestionService} from "../common/firebase/services/question.service";
-import { Observable, Subscription } from 'rxjs/Rx';
+import {QuizResultService} from "../common/firebase/services/quizresult.service";
+import {Observable} from "rxjs/Rx";
+import {QuizResult, ResultAnswer} from "../common/model/quizresult.model";
 
 @Component({
   inputs: ['user', 'quiz'],
@@ -23,10 +25,12 @@ export class PlayerComponent extends MaterialComponent implements OnInit {
   state: string = 'created';
   started: Date = null;
   timeLeft: number = null;
+  private result: QuizResult;
 
   constructor(protected uiRouter: UIRouter,
               private topicService: TopicService,
               private questionService: QuestionService,
+              private quizResultService: QuizResultService,
               private chRef: ChangeDetectorRef) {
     super();
   }
@@ -48,18 +52,31 @@ export class PlayerComponent extends MaterialComponent implements OnInit {
   }
 
   start() {
-    debugger;
     this.state = 'started';
     this.started = new Date();
+    this.result = new QuizResult();
+    this.result.start = this.started;
+    this.result.quiz = this.quiz.id;
+    this.result.user = this.user.email;
+    this.quizResultService.create(this.result).then((res) => this.result = res);
     if(this.quiz.totalTimeLimit) {
-      this.timeLeft = this.quiz.totalTimeLimit * 60 * 1000 - (Date.now() - this.started.getTime())
+      this.timeLeft = this.quiz.totalTimeLimit * 60 * 1000 - (Date.now() - this.started.getTime());
       Observable.interval(1000).subscribe(() => {
-        this.timeLeft = this.quiz.totalTimeLimit * 60 * 1000 - (Date.now() - this.started.getTime())
+        this.timeLeft = this.quiz.totalTimeLimit * 60 * 1000 - (Date.now() - this.started.getTime());
         if(this.timeLeft<=0) {
           this.complete();
         }
       });
     }
+  }
+
+  answer(question: Question, entered: string[]) {
+    let res = new ResultAnswer();
+    res.question = question.id;
+    res.entered = entered;
+    this.result.answers.push(res);
+    this.quizResultService.update(this.result).then(() => {
+    });
   }
   complete() {
     this.state = 'completed';
