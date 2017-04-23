ca3b3e2d6e46e1de97f2eb0606faeb6596051145
Vladislav Zubairov <vzubairov@ankocorp.com>
2017-04-23 20:06:15 +0700

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap-material-design/dist/js/material.js";
 import "bootstrap-material-design/dist/js/ripples.js";
+import "dropdown.js";
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
